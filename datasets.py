2255d6a02e205624396a002cf9f2c3ba418717b6
Sundance <77976941+sundanc@users.noreply.github.com>
2024-12-22 17:36:56 +0300

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+path = kagglehub.dataset_download("kartik2112/fraud-detection")
 
 print("Path to dataset files:", path)
